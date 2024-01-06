1b3efd48c7b1c53992d8d9ac17a03d9495ceb4be
Alexandros Kritikos <kritikosal1996@gmail.com>
2024-01-06 20:57:38 +0200

    refactoring and change messages to greek

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord
 import os
 import random
-import utils
 import asyncio
+import utils
 
 from discord.ext import commands
 from discord.ext.tasks import loop
@@ -11,7 +11,7 @@ from discord.utils import get
 from dotenv import load_dotenv
 from newsapi import NewsApiClient
 from datetime import date, timedelta
-
+from constants import *
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,7 +19,7 @@ intents.members = True
 
 # instantiate discord client 
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 newsapi = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
 
 '''
@@ -30,36 +30,36 @@ load_dotenv()
 
 load_dotenv()
 
-@loop(seconds=30)
+@loop(seconds=MEMBER_COUNT_LOOP_SECONDS)
 async def check_all_members():
-  guild = bot.get_guild(1140325411126005901) # Koutokomia
+  guild = bot.get_guild(GUILD_ID)
   MembersInServerCount = len(guild.members)
   bots = list(filter(utils.filter_bots, guild.members))
-  channel = get(guild.channels, id=1192792733039996938) # Ψυχολογικά όντα
+  channel = get(guild.channels, id=MEMBER_COUNT_CHANNEL_ID)
   amount = (MembersInServerCount - len(bots))
-  prevtname = str(f'{str("Souls: ")}{amount}')
-  tname = channel.name
-  if prevtname == tname:
+  new_channel_name = str(f'{str(MEMBER_COUNT_CHANNEL_PREFIX)}{amount}')
+  current_channel_name = channel.name
+  if new_channel_name == current_channel_name:
     pass
-  elif prevtname != tname:
-    await channel.edit(name=f'{str("Souls: ")}{amount}')
+  elif new_channel_name != current_channel_name:
+    await channel.edit(name=new_channel_name)
 
 @bot.command()
 async def list_audio(ctx):
-  await ctx.send("Here is a list of the available audio files:\n\t" 
-                 + "\n\t".join(os.listdir("./audio")) 
-                 + "\nTo play a file, copy-paste a file name from above and type:\n"
-                 + "$play <song-name>")
+  await ctx.send("Ακολουθεί η λίστα με τις διαθέσιμες ηχογραφήσεις:\n\t" 
+                 + "\n\t".join(os.listdir(AUDIO_DIR)) 
+                 + "\nΓια αναπαραγωγή ηχογράφησης, αντέγραψε το όνομα της ηχογράφησης από τις παραπάνω και γράψε:\n"
+                 + "\t$play <το-όνομα-της-ηχογράφησης>")
 
 @bot.command()
 async def play(ctx, arg):
   if not ctx.message.author.voice:
-    await ctx.send("My apologies master {}, but you must be in a voice channel so that I can join you.".format(ctx.message.author.name))
+    await ctx.send("Ζητώ συγνώμη δάσκαλε {}, αλλά πρέπει να είσαι σε voice channel για να κάνω join.".format(ctx.message.author.name))
     return
   else:
     channel = ctx.message.author.voice.channel
     voice = await channel.connect()
-    source = FFmpegPCMAudio("./audio/" + arg)
+    source = FFmpegPCMAudio(AUDIO_DIR + arg)
     voice.play(source)
 
 @bot.command()
@@ -68,7 +68,7 @@ async def pause(ctx):
   if voice.is_playing():
     voice.pause()
   else:
-    await ctx.send("There is no audio to pause right now.")
+    await ctx.send("Δεν ακούω κάποια ηχογράφηση για να μπορώ να την κάνω pause.")
 
 @bot.command()
 async def resume(ctx):
@@ -76,7 +76,7 @@ async def resume(ctx):
   if voice.is_paused():
     voice.resume()
   else:
-    await ctx.send("There is no paused audio right now.")
+    await ctx.send("Παίζει ήδη μια ηχογράφηση, οπότε με τρόπο αγνοώ την εντολή resume.")
 
 @bot.command()
 async def stop(ctx):
@@ -88,20 +88,20 @@ async def leave(ctx):
   if ctx.voice_client:
     await ctx.guild.voice_client.disconnect()
   else:
-    await ctx.send("I do not want to offend you master {}, but you must be seeing hallucinations, because I am not in a voice channel at the moment.".format(ctx.message.author.name))
+    await ctx.send("Δεν θέλω να σε προσβάλω δάσκαλε {}, αλλά φαίνεται ότι βλέπεις παραισθήσεις, επειδή δεν είμαι σε voice channel αυτή τη στιγμή.".format(ctx.message.author.name))
 
 # discord event to check when the bot is online 
 @bot.event
 async def on_ready():
-  await bot.change_presence(activity=discord.Game("NieR:Automata™"))
+  await bot.change_presence(activity=discord.Game(PLAYING_ACTIVITY))
   check_all_members.start()
-  await asyncio.sleep(1)
+  await asyncio.sleep(ONE)
   print(f"{bot.user} is now online!")
 
 @bot.event
 async def on_member_join(member):
-  channel = bot.get_channel(1140325411126005903) # ίντριγκα
-  await channel.send(f"{member.mention} " + utils.to_multi_line_text("welcome.txt"))
+  channel = bot.get_channel(SYSTEM_CHANNEL_ID)
+  await channel.send(f"{member.mention} " + utils.to_multi_line_text(WELCOME_FILE))
 
 @bot.event
 async def on_message(message): 
@@ -113,35 +113,35 @@ async def on_message(message):
   # lower case message
   message_content = message.content.lower()  
   
-  if "free" in message_content and message.channel.id == 1190706436683071648: #free-games
-    await message.add_reaction("\U0001F42D")
+  if FREE_STR in message_content and FREE_GAMES_CHANNEL_ID == message.channel.id:
+    await message.add_reaction(FREE_GAMES_REACTION)
 
-  if message_content.startswith(f"{bot.user.mention} $help"):
-    await message.channel.send(utils.to_multi_line_text("help.txt"))
+  if message_content.startswith(f"{bot.user.mention} " + HELP_COMMAND):
+    await message.channel.send(utils.to_multi_line_text(HELP_FILE))
 
-  if message_content.startswith(f"{bot.user.mention} $real_name"):
-    await message.channel.send("Claire Elise Boucher")
+  if message_content.startswith(f"{bot.user.mention} " + REAL_NAME_COMMAND):
+    await message.channel.send(REAL_NAME_RESPONSE)
 
-  if message_content.startswith(f"{bot.user.mention} $greet"):
-    await message.channel.send(f"Master {message.author.name}, it is an honor to serve as your informant. On behalf of our Creators, I will do my best.")
+  if message_content.startswith(f"{bot.user.mention} " + GREET_COMMAND):
+    await message.channel.send(f"Δάσκαλε {message.author.name}, είναι τιμή μου που σε υπηρετώ. Εκ μέρους των Δημιουργών μας, θα κάνω ό,τι καλύτερο μπορώ.")
 
-  if message_content.startswith(f"{bot.user.mention} $retrieve"):
-    news_results = newsapi.get_everything(q="grimes",
-                                          qintitle="grimes",
-                                          sources="business-insider",
+  if message_content.startswith(f"{bot.user.mention} " + RETRIEVE_COMMAND):
+    news_results = newsapi.get_everything(q=GRIMES_STR,
+                                          qintitle=GRIMES_STR,
+                                          sources=BUSINESS_INSIDER,
                                           from_param=date.today() - timedelta(days=int(os.getenv("NEWS_API_DAYS_BEFORE_CURRENT"))),
-                                          sort_by="publishedAt",
+                                          sort_by=PUBLISHED_AT,
                                           page_size=int(os.getenv("NEWS_API_PAGE_SIZE")))  
 
-    if len(news_results) > 0:
-      for article in news_results["articles"]:
-        if "grimes" in article["title"].lower():
-          await message.channel.send(article["url"])
+    if len(news_results) > ZERO:
+      for article in news_results[ARTICLES]:
+        if GRIMES_STR in article[TITLE].lower():
+          await message.channel.send(article[URL_STR])
     else:
-      await message.channel.send(f"My apologies master {message.author.name}, I was not able to find anything. Please try a few cycles later.")
+      await message.channel.send(f"Ζητώ συγνώμη δάσκαλε {message.author.name}, δεν κατάφερα να βρω κάτι. Παρακαλώ δοκίμασε μερικούς κύκλους αργότερα.")
 
-  if message_content.startswith(f"{bot.user.mention} $fact"):
-    facts = open("facts.txt").read().splitlines()
+  if message_content.startswith(f"{bot.user.mention} " + FACT_COMMAND):
+    facts = open(FACTS_FILE).read().splitlines()
     random_fact = random.choice(facts)
     await message.channel.send(random_fact)
 
